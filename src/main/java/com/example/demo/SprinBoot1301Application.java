989e40a58cf5bc418d0d2be2b85fdad9b0ec8836
Melkamu8 <melkamudi@yahoo.com>
2020-01-09 16:06:46 -0500

    lesson_301

---
+++ src/main/java/com/example/demo/SprinBoot1301Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBoot1301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinBoot1301Application.class, args);
+    }
+
+}
